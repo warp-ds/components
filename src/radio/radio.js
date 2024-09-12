42fcd16c65927325897e3ed2f9936d90850cd3ea
William Killerud <william.killerud@schibsted.com>
2024-09-12 11:31:37 +0200

    hmm

--- src/radio/radio.js
+++ src/radio/radio.js
@@ -5,53 +5,100 @@ import styles from "./radio-styles.js";
 import sharedStyles from "../sharedStyles.js";
 
 export class WarpRadio extends LitElement {
-  static get formAssociated() {
-    return true;
-  }
+  static formAssociated = true;
 
+  #internals;
+  
   constructor() {
     super();
-    this.internals = this.attachInternals();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.#internals = this.attachInternals();
   }
 
   static styles = [styles, sharedStyles];
 
-  static properties = {
-    checked: { type: Boolean, reflect: true },
-    disabled: { type: Boolean, reflect: true },
-    invalid: { type: Boolean, reflect: true },
-    required: { type: Boolean, reflect: true },
-    name: { type: String },
-  };
-
-  handleChange(event) {
-    this.checked = event.target.checked;
-    const customEvent = new CustomEvent(event.type, event);
-    console.log("handleChange", customEvent);
-
-    this.dispatchEvent(customEvent);
+  get form() {
+    return this.#internals.form;
+  }
+  
+  get name() {
+    return this.#internals.form?.name;
   }
 
-  handleInput(event) {
-    this.checked = event.target.checked;
-    console.log("handleInput", event);
-    this.dispatchEvent(
-      new CustomEvent("toggleSelected", {
-        bubbles: true,
-        composed: true,
-        target: this,
-      })
-    );
+  set name(v) {
+    if (this.#internals.form) {
+      this.#internals.form.name = v;
+    }
   }
 
-  firstUpdated(...args) {
-    super.firstUpdated(...args);
-    /** This ensures our element always participates in the form */
-    this.internals.setFormValue(this.value);
+  get checked() {
+    return this.#internals.form?.checked;
+  }
+
+  set checked(v) {
+    if (!this.#internals.form) return;
+    this.#internals.form.checked = v;
+  }
+  
+  set value(v) {
+    this.#internals.setFormValue(v);
+  }
+  
+  get value() {
+    return this.#internals.form?.value;
+  }
+
+  get type() { return this.localName; }
+
+  get validity() {
+    return this.#internals.validity;
+  }
+  
+  get validationMessage() {
+    return this.#internals.validationMessage;
+  }
+  
+  get willValidate() {
+    return this.#internals.willValidate;
+  }
+  
+  checkValidity() {
+    return this.#internals.checkValidity();
+  }
+  
+  reportValidity() {
+    return this.#internals.reportValidity();
+  }
+
+  // handleChange(event) {
+  //   this.checked = event.target.checked;
+  //   const customEvent = new CustomEvent(event.type, event);
+  //   console.log("handleChange", customEvent);
+
+  //   this.dispatchEvent(customEvent);
+  // }
+
+  // handleInput(event) {
+  //   this.checked = event.target.checked;
+  //   this.dispatchEvent(
+  //     new CustomEvent("toggleSelected", {
+  //       bubbles: true,
+  //       composed: true,
+  //       target: this,
+  //     })
+  //   );
+  // }
+
+  // firstUpdated(...args) {
+  //   super.firstUpdated(...args);
+  //   /** This ensures our element always participates in the form */
     
-    /** Make sure validations are set up */
-    //this._manageRequired();
-  }
+  //   /** Make sure validations are set up */
+  //   //this._manageRequired();
+  // }
 
   render() {
     const wrapperClasses = classMap({
@@ -61,14 +108,12 @@ export class WarpRadio extends LitElement {
     return html`<div class="w-radio ${wrapperClasses}" part="w-radio-part">
       <input
         type="radio"
-        ?disabled="${this.disabled}"
-        ?checked="${this.checked}"
-        @input="${this.handleInput}"
-        @change="${this.handleChange}"
+        ?disabled="${this.#internals.form?.disabled}"
+        ?checked="${this.#internals.form?.checked}"
         aria-required="${Boolean(this.required)}"
         id="w-c-radio__input"
         class="w-sr-only"
-        .name="${ifDefined(this.name)}"
+        .name="${this.#internals.form?.name}"
       />
       <label for="w-c-radio__input"><slot></slot></label>
     </div>`;

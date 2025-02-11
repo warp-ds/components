4d1dba00fd4132c06759b2bc7c428e3e55e05b43
imprashast <imprashast@gmail.com>
2025-02-11 11:12:41 +0100

    Add biome to the project

--- react/button/index.js
+++ react/button/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createComponent } from '@lit/react';
-import { WarpButton as WarpButtonWC } from '../../src/button/index.js';
+import { createComponent } from "@lit/react";
+import React from "react";
+import { WarpButton as WarpButtonWC } from "../../src/button/index.js";
 
 export const WarpButton = createComponent({
-  tagName: 'w-button',
-  elementClass: WarpButtonWC,
-  react: React,
-  events: {
-    onClick: 'click',
-  },
+	tagName: "w-button",
+	elementClass: WarpButtonWC,
+	react: React,
+	events: {
+		onClick: "click",
+	},
 });
--- src/icon/cache-fetch.js
+++ src/icon/cache-fetch.js
@@ -2,7 +2,7 @@ const _fetchMap = new Map();
 
 /**
  * A callback to parse Response body.
- * 
+ *
  * @callback ResponseParser
  * @param {Fetch.Response} response
  * @returns {Promise}
@@ -10,18 +10,19 @@ const _fetchMap = new Map();
 
 /**
  * A minimal in-memory map to de-duplicate Fetch API media requests.
- * 
+ *
  * @param {String} uri
  * @param {Object} [options={}]
  * @param {ResponseParser} [options.responseParser=(response) => response.text()]
  * @returns {Promise}
  */
 const cacheFetch = (uri, options = {}) => {
-  const responseParser = options.responseParser || ((response) => response.text());
-  if (!_fetchMap.has(uri)) {
-    _fetchMap.set(uri, fetch(uri).then(responseParser));
-  }
-  return _fetchMap.get(uri);  
-}
+	const responseParser =
+		options.responseParser || ((response) => response.text());
+	if (!_fetchMap.has(uri)) {
+		_fetchMap.set(uri, fetch(uri).then(responseParser));
+	}
+	return _fetchMap.get(uri);
+};
 
 export default cacheFetch;

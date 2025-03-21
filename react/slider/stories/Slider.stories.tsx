5cc17fcb4390e9758d029c462b5bdba93b59fd46
adam-zawisza-jalaluddin-ahmed <adam.zawisza.jalaluddin.ahmed@schibsted.com>
2025-03-21 15:39:25 +0100

    add story

--- react/slider/stories/Slider.stories.tsx
+++ react/slider/stories/Slider.stories.tsx
@@ -112,6 +112,18 @@ export const RegularWithStartEndValues = () => {
   );
 };
 
+export const RegularOnChangeAfter = () => {
+  const [value, setValue] = useState(20000);
+
+  return (
+    <div>
+      <output>{value}</output>
+      <Slider onChangeAfter={(value) => setValue(value)} value={value} min={10000} max={60000} />
+      <button onClick={() => setValue(15000)}>Reset</button>
+    </div>
+  );
+};
+
 export const WithRangeValues = () => {
   const range = [
     { label: '12:00' },

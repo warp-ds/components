59fdecfa67a269bc30684ec4727f0863a25bc97c
adam-zawisza-jalaluddin-ahmed <adam.zawisza.jalaluddin.ahmed@schibsted.com>
2025-03-21 10:51:48 +0100

    update stories

--- react/slider/stories/Slider.stories.tsx
+++ react/slider/stories/Slider.stories.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Slider } from '../src/Slider.tsx';
+import { TextField } from '@warp-ds/react/components/textfield';
 
 const metadata = { title: 'Forms/Slider' };
 export default metadata;
@@ -8,170 +9,115 @@ export const Regular = () => {
   const [value, setValue] = useState(20000);
 
   return (
-    <div style={{ maxWidth: '500px' }}>
+    <div>
       <output>{value}</output>
-      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} step={10} />
+      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} />
       <button onClick={() => setValue(15000)}>Reset</button>
     </div>
   );
 };
 
-export const Regular1 = () => {
-  const [value, setValue] = useState(10000);
+export const RegularWithInput = () => {
+  const [value, setValue] = useState(500);
 
   return (
-    <div style={{ maxWidth: '500px' }}>
+    <div>
       <output>{value}</output>
-      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} step={10} />
-      <button onClick={() => setValue(15000)}>Reset</button>
+      <Slider onChange={(value) => setValue(value)} value={value} min={1} max={1000} />
+      <div style={{ width: '50%', marginBottom: '10px' }}>
+        <TextField value={value.toString()} onChange={(e) => setValue(+e.target.value)} />
+      </div>
+      <button onClick={() => setValue(500)}>Reset</button>
     </div>
   );
 };
 
-export const Regular2 = () => {
+export const RegularStep1000 = () => {
   const [value, setValue] = useState(20000);
 
   return (
-    <div style={{ maxWidth: '500px' }}>
+    <div>
+      <output>{value}</output>
+      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} step={1000} />
+      <button onClick={() => setValue(15000)}>Reset</button>
+    </div>
+  );
+};
+
+export const RegularWithContainedMarks = () => {
+  const [value, setValue] = useState(20000);
+
+  return (
+    <div>
+      <output>{value}</output>
+      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} containMarks />
+      <button onClick={() => setValue(15000)}>Reset</button>
+    </div>
+  );
+};
+
+export const RegularWithoutMarks = () => {
+  const [value, setValue] = useState(20000);
+
+  return (
+    <div>
+      <output>{value}</output>
+      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} showMarks={false} />
+      <button onClick={() => setValue(15000)}>Reset</button>
+    </div>
+  );
+};
+
+export const RegularWithTooltips = () => {
+  const [value, setValue] = useState(20000);
+
+  return (
+    <div>
+      <output>{value}</output>
+      <Slider onChange={(value) => setValue(value)} value={value} min={10000} max={60000} showTooltips containMarks />
+      <button onClick={() => setValue(15000)}>Reset</button>
+    </div>
+  );
+};
+
+export const RegularWithStartEndValues = () => {
+  const [value, setValue] = useState(20000);
+
+  return (
+    <div>
       <output>{value}</output>
       <Slider
         onChange={(value) => setValue(value)}
         value={value}
         min={10000}
         max={60000}
+        startEndValues={['Less than 10 000', 'Over 60 000']}
         showTooltips
-        showMarks
-        step={100}
-      />
-      <button onClick={() => setValue(15000)}>Reset</button>
-    </div>
-  );
-};
-
-export const Regular3 = () => {
-  const [value, setValue] = useState(20000);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider
-        onChange={(value) => setValue(value)}
-        value={value}
-        min={10000}
-        max={60000}
-        showTooltips
-        showMarks
-        step={100}
-      />
-      <button onClick={() => setValue(15000)}>Reset</button>
-    </div>
-  );
-};
-
-export const Regular4 = () => {
-  const [value, setValue] = useState(3);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider onChange={(value) => setValue(value)} value={value} min={0} max={6} showTooltips showMarks />
-      <button onClick={() => setValue(3)}>Reset</button>
-    </div>
-  );
-};
-
-export const Regular5 = () => {
-  const [value, setValue] = useState(3);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider onChange={(value) => setValue(value)} value={value} min={1} max={8} showTooltips showMarks />
-      <button onClick={() => setValue(3)}>Reset</button>
-    </div>
-  );
-};
-
-export const Regular6 = () => {
-  const [value, setValue] = useState(3);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider
-        onChange={(value) => setValue(value)}
-        value={value}
-        min={2}
-        max={10}
-        showTooltips
-        showMarks
         containMarks
-        startEndValues={['Less than 2', 'gt 10']}
       />
-      <button onClick={() => setValue(3)}>Reset</button>
+      <button onClick={() => setValue(15000)}>Reset</button>
     </div>
   );
 };
 
-export const Regular7 = () => {
-  const [value, setValue] = useState(3);
+export const WithRangeValues = () => {
+  const range = [
+    { label: '12:00' },
+    { label: '12:30' },
+    { label: '13:00' },
+    { label: '13:30' },
+    { label: '14:00' },
+    { label: '14:30' },
+    { label: '15:00' },
+  ];
+
+  const [value, setValue] = useState({ label: '12:30' });
 
   return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider
-        onChange={(value) => setValue(value)}
-        value={value}
-        min={2}
-        max={10}
-        showTooltips
-        showMarks
-        startEndValues={[null, 'gt 10']}
-      />
-      <button onClick={() => setValue(3)}>Reset</button>
-    </div>
-  );
-};
-
-export const RegularRangeValues = () => {
-  const [value, setValue] = useState('yes');
-
-  const range = ['hi', 'haha', 'yes', 'nr', 'ok', '1', '2'];
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider onChange={(value) => setValue(value)} value={value} rangeValues={range} showTooltips showMarks />
-      <button onClick={() => setValue('yes')}>Reset</button>
-    </div>
-  );
-};
-
-export const Disabled = () => {
-  const [value, setValue] = useState(625_000);
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{value}</output>
-      <Slider onChange={(val) => setValue(val)} value={value} disabled min={1000} max={10_000_000} step={1000} />
-    </div>
-  );
-};
-
-export const ChangeAfter = () => {
-  const [value, setValue] = useState(625_000);
-  const [valueAfter, setValueAfter] = useState(value);
-
-  return (
-    <div style={{ maxWidth: '500px' }}>
-      <output>{valueAfter}</output>
-      <Slider
-        onChange={(val) => setValue(val)}
-        onChangeAfter={(val) => setValueAfter(val)}
-        value={value}
-        min={1000}
-        max={10_000_000}
-        step={1000}
-      />
+    <div>
+      <output>{value.label}</output>
+      <Slider onChange={(value) => setValue(value)} rangeValues={range} value={value} showTooltips containMarks />
+      <button onClick={() => setValue({ label: '12:30' })}>Reset</button>
     </div>
   );
 };

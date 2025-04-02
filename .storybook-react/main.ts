64ef2554ef0cb54c473bff6f5638c2d019bcc2a0
zara.melyan@schibsted.com <zara.melyan@schibsted.com>
2025-04-02 15:15:02 +0200

    feat(storybook): attempt at adding brand themes to storybook-react

--- .storybook-react/main.ts
+++ .storybook-react/main.ts
@@ -1,17 +1,31 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
+import Unocss from "unocss/vite";
+import { presetWarp } from "@warp-ds/uno";
 
 const config: StorybookConfig = {
-  stories: ['../react/**/*.stories.[tj]sx'],
+  stories: ["../react/**/*.stories.@(tsx|jsx)"],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
+  viteFinal: async (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins.push(
+      Unocss({
+        presets: [presetWarp()],
+      })
+    );
+
+    return config;
+  },
 };
+
 export default config;

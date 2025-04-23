1b03fd67da10c3b3c64c3df3988eb6f30e994076
imprashast <imprashast@gmail.com>
2025-04-23 14:24:19 +0200

    refactor: Clean up Storybook configuration and remove unused dependencies

--- .storybook-react/main.ts
+++ .storybook-react/main.ts
@@ -1,30 +1,17 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-import Unocss from "unocss/vite";
-import { presetWarp } from "@warp-ds/uno";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../react/**/*.stories.@(tsx|jsx)"],
+  stories: ['../react/**/*.stories.@(tsx|jsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
-  },
-  viteFinal: async (config) => {
-    config.plugins = config.plugins || [];
-
-    config.plugins.push(
-      Unocss({
-        presets: [presetWarp()],
-      })
-    );
-
-    return config;
   },
 };
 
--- .storybook-react/preview.ts
+++ .storybook-react/preview.ts
@@ -1,49 +1,44 @@
-import type { Preview } from "@storybook/react";
-import "uno.css"; // injects styles generated from your presetWarp
+import type { Preview } from '@storybook/react';
 
 export const themes = {
-  "Finn light": "finn-no",
-  "Finn dark": "finn-no-dark",
-  "Tori light": "tori-fi",
-  "Tori dark": "tori-fi-dark",
-  "DBA light": "dba-dk",
-  "DBA dark": "dba-dk-dark",
-  "Blocket light": "blocket-se",
-  "Blocket dark": "blocket-se-dark",
+  'Finn light': 'finn-no',
+  'Finn dark': 'finn-no-dark',
+  'Tori light': 'tori-fi',
+  'Tori dark': 'tori-fi-dark',
+  'DBA light': 'dba-dk',
+  'DBA dark': 'dba-dk-dark',
+  'Blocket light': 'blocket-se',
+  'Blocket dark': 'blocket-se-dark',
 };
 
 export const rewriteStylesheets = (theme) => {
+  console.log('rewriteStylesheets', theme);
   const roots = [
     document,
-    ...Array.from(document.querySelectorAll("*"))
+    ...Array.from(document.querySelectorAll('*'))
       .filter((el) => !!el.shadowRoot)
       .map((el) => el.shadowRoot),
   ];
   roots.forEach((root) => {
     //@ts-ignore
-    const stylesheets = root.querySelectorAll(
-      'link[rel="stylesheet"][href*="/@warp-ds/css/"]'
-    );
-    stylesheets.forEach((s) => {
-      s.setAttribute(
-        "href",
-        `https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/${theme}.css`
-      );
+    const tokensStylesheets = root.querySelectorAll('link[rel="stylesheet"][href*="@warp-ds/css/v2/tokens/"]');
+    tokensStylesheets.forEach((s) => {
+      s.setAttribute('href', `https://assets.finn.no/pkg/@warp-ds/css/v2/tokens/${theme}.css`);
     });
   });
 };
 
 export const globalTypes = {
   brand: {
-    name: "Brand Theme",
-    description: "Select a design system brand",
-    defaultValue: "finn-no", // fallback if no localStorage
+    name: 'Brand Theme',
+    description: 'Select a design system brand',
+    defaultValue: 'finn-no', // fallback if no localStorage
     toolbar: {
-      icon: "globe",
+      icon: 'globe',
       items: Object.entries(themes).map(([label, value]) => ({
         value,
         title: label,
-        right: value.includes("dark") ? "🌙" : "☀️",
+        right: value.includes('dark') ? '🌙' : '☀️',
       })),
     },
   },
@@ -54,7 +49,7 @@ export const decorators = [
     const theme = context.globals.brand;
 
     // Persist theme
-    localStorage.setItem("warpTheme", theme);
+    localStorage.setItem('warpTheme', theme);
 
     // Swap stylesheet
     rewriteStylesheets(theme);
@@ -63,8 +58,7 @@ export const decorators = [
   },
 ];
 
-const defaultTheme =
-  typeof window !== "undefined" && localStorage.getItem("warpTheme");
+const defaultTheme = typeof window !== 'undefined' && localStorage.getItem('warpTheme');
 if (defaultTheme) {
   rewriteStylesheets(defaultTheme);
 }

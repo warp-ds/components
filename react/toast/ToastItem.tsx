8e8d4fd2dea2bea7299679a27c10dd0b64ad7a79
zara.melyan@schibsted.com <zara.melyan@schibsted.com>
2025-05-08 16:31:21 +0200

    feat: Refactor toast component structure and improve functionality with new storage handling

--- react/toast/ToastItem.tsx
+++ react/toast/ToastItem.tsx
@@ -1,28 +1,29 @@
-import style from 'inline:./toast.styles.css';
-import IconClose16 from '@warp-ds/icons/react/close-16';
-import classNames from 'classnames';
-import { createRef } from 'react';
-import { Button } from '../button/Button.tsx';
-import { toastIcon, toastRole, toastVariantClassMap } from './helpers.tsx';
-import { ToastProps } from './toast.types.ts';
-import { useToast } from './useToast.ts';
+import style from "inline:./styles.css";
+import IconClose16 from "@warp-ds/icons/react/close-16";
+import classNames from "classnames";
+import { Button } from "../button/Button.tsx";
+import { toastConfig } from "./utils.tsx";
+import { ToastProps } from "./props.ts";
+import { removeToast } from "./ToastContainer.tsx";
 
 /* 
   TODO: REMOVE TEMP STYLES, TEST BROWSERS, TEST WEB/MOBILE
-  TODO: array vs map
   TODO: Test all 3 variants simultaneously and see if the styles override each other
   TODO: Check button component usage!! Currently using button overlayQuiet with custom padding
   Q: Is the component self-contained?
   */
 
-const ToastItem = ({ variant, text, dismissible = false, id }: ToastProps) => {
-  const { removeToast } = useToast();
-  const buttonRef = createRef<HTMLButtonElement>();
-  const VariantIcon = toastIcon[variant];
+const ToastItem = ({
+  variant,
+  text,
+  dismissible = false,
+  id,
+}: ToastProps) => {
+  const { icon: Icon, role, className } = toastConfig[variant];
 
-  const toastClasses = classNames('w-toast__item', toastVariantClassMap[variant], {
+  const toastClasses = classNames("w-toast__item", className, {
     // TODO: check that this class exists
-    'w-toast--dismissible': dismissible,
+    "w-toast--dismissible": dismissible,
   });
 
   return (
@@ -30,14 +31,22 @@ const ToastItem = ({ variant, text, dismissible = false, id }: ToastProps) => {
       <style>{style}</style>
       <div
         className={toastClasses} // --warning, --success, --negative, --dismissible
-        role={toastRole[variant]}
+        role={role}
+        id={id}
       >
         <span className="w-toast__icon">
-          <VariantIcon />
+          <Icon />
         </span>
         <p>{text}</p>
         {dismissible && (
-          <Button variant="overlayQuiet" size="small" className="p-4" onClick={() => removeToast(id)}>
+          <Button
+            variant="overlayQuiet"
+            size="small"
+            //TODO: remove ts-ignore when button branch is merged
+            //@ts-ignore
+            hasIconOnly
+            onClick={() => removeToast(id)}
+          >
             <IconClose16 />
           </Button>
         )}
--- react/toast/props.ts
+++ react/toast/props.ts
@@ -3,7 +3,7 @@ export type ToastDuration = 5000 | 8000 | 10000;
 export type ToastVariant = 'success' | 'warning' | 'negative';
 
 // TODO: decide on and change type of id
-export type ToastId = any;
+export type ToastId = string;
 
 export type ToastProps = {
   variant: ToastVariant;

22de4ac8b2c15f1b44fbbef59769c96643bbf126
Snorre Nygaard <snorre.nygaard@finn.no>
2025-05-13 08:52:02 +0200

    Fix ticks positioning

--- react/slider-v2/Marks.tsx
+++ react/slider-v2/Marks.tsx
@@ -2,8 +2,10 @@ export const getMarks = (min: number, max: number) => (
   <div className="w-slider__ticks">
     <div className="w-slider__tick w-slider__ticks--left" />
     <div className="w-slider__tick w-slider__ticks--right" />
-    <div className="w-slider__label w-slider__ticks--left">{min}</div>
-    <div className="w-slider__label w-slider__ticks--right">
+    <div className="w-slider__min-value w-slider__ticks--left">
+      {min}
+    </div>
+    <div className="w-slider__max-value w-slider__ticks--right">
       {max}
     </div>
   </div>

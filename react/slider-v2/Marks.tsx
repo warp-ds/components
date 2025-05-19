af0d2c565fb896b03de18ef5f255ec01ad079616
Snorre Nygaard <snorre.nygaard@finn.no>
2025-05-19 12:09:52 +0200

    remove ticks

--- react/slider-v2/Marks.tsx
+++ react/slider-v2/Marks.tsx
@@ -1,7 +1,5 @@
 export const getMarks = (min: number, max: number) => (
   <div className="w-slider__ticks">
-    <div className="w-slider__tick w-slider__ticks--left" />
-    <div className="w-slider__tick w-slider__ticks--right" />
     <div className="w-slider__min-value w-slider__ticks--left">
       {min}
     </div>

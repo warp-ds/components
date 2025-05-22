20d78b6096c653c92c4812884d53ca2d0f7dc969
Snorre Nygaard <snorre.nygaard@finn.no>
2025-05-22 08:17:43 +0200

    Add Correct styling for regular and range slider

--- react/slider-v2/Slider.stories.tsx
+++ react/slider-v2/Slider.stories.tsx
@@ -10,24 +10,27 @@ export const Regular = () => {
   const [value, setValue] = useState(0);
   const [output, setOutput] = useState(value);
   return (
-    <div style={{ width: '320px' }}>
-      <RegularSlider
-        onChange={(value) => setValue(value)}
-        onInput={setOutput}
-        min={0}
-        max={100}
-        step={10}
-        value={value}
-        showMarks={true}
-      />
-      <div className="w-slider__input">
-        <TextField
-          label="value"
-          value={value.toString()}
-          onChange={(e) => setValue(+e.target.value)}
+    <>
+      <output>{output}</output>
+      <div style={{ width: '320px', outline: '1px dashed red' }}>
+        <RegularSlider
+          onChange={(value) => setValue(value)}
+          onInput={setOutput}
+          min={0}
+          max={100}
+          step={10}
+          value={value}
+          showMarks={true}
         />
+        <div className="w-slider__input">
+          <TextField
+            label="value"
+            value={value.toString()}
+            onChange={(e) => setValue(+e.target.value)}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -35,26 +38,28 @@ export const DisabledRegular = () => {
   const [value, setValue] = useState(0);
   const [output, setOutput] = useState(value);
   return (
-    <div>
+    <>
       <output>{output}</output>
-      <RegularSlider
-        disabled
-        onChange={(value) => setValue(value)}
-        onInput={setOutput}
-        min={0}
-        max={100}
-        step={10}
-        value={value}
-      />
-      <div className="w-slider__input">
-        <TextField
-          label="value"
+      <div style={{ width: '320px', outline: '1px dashed red' }}>
+        <RegularSlider
           disabled
-          value={value.toString()}
-          onChange={(e) => setValue(+e.target.value)}
+          onChange={(value) => setValue(value)}
+          onInput={setOutput}
+          min={0}
+          max={100}
+          step={10}
+          value={value}
         />
+        <div className="w-slider__input">
+          <TextField
+            label="value"
+            disabled
+            value={value.toString()}
+            onChange={(e) => setValue(+e.target.value)}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -82,39 +87,43 @@ export const Ranged = () => {
   };
 
   return (
-    <div>
+    <>
       <output>
         {output[0]} - {output[1]} kr
       </output>
-      <RangeSlider
-        onChange={setValue}
-        onInput={setOutput}
-        min={1000000}
-        max={10000000}
-        step={50000}
-        value={value}
-      />
-      <div className="w-slider__input">
-        <TextField
-          label="Min"
-          value={value[0].toString()}
-          type="number"
-          invalid={errorMin}
-          helpText={errorMin ? 'Min cannot be greater than max' : ''}
-          onBlur={onBlurMin}
-          onChange={(e) => setValue([+e.target.value, value[1]])}
+      <div style={{ width: '320px', outline: '1px dashed red' }}>
+        <RangeSlider
+          onChange={setValue}
+          onInput={setOutput}
+          min={1000000}
+          max={10000000}
+          step={50000}
+          value={value}
         />
-        <TextField
-          label="Max"
-          value={value[1].toString()}
-          type="number"
-          invalid={errorMax}
-          helpText={errorMax ? 'Max cannot be lower than min' : ''}
-          onBlur={onBlurMax}
-          onChange={(e) => setValue([value[0], +e.target.value])}
-        />
+        <div className="w-slider__input">
+          <TextField
+            label="Min"
+            value={value[0].toString()}
+            type="number"
+            invalid={errorMin}
+            helpText={
+              errorMin ? 'Min cannot be greater than max' : ''
+            }
+            onBlur={onBlurMin}
+            onChange={(e) => setValue([+e.target.value, value[1]])}
+          />
+          <TextField
+            label="Max"
+            value={value[1].toString()}
+            type="number"
+            invalid={errorMax}
+            helpText={errorMax ? 'Max cannot be lower than min' : ''}
+            onBlur={onBlurMax}
+            onChange={(e) => setValue([value[0], +e.target.value])}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -122,33 +131,35 @@ export const RangedSimple = () => {
   const [value, setValue] = useState<[number, number]>([0, 100]);
   const [output, setOutput] = useState(value);
   return (
-    <div>
+    <>
       <output>
         {output[0]} - {output[1]} kr
       </output>
-      <RangeSlider
-        onChange={setValue}
-        onInput={setOutput}
-        min={0}
-        max={100}
-        step={5}
-        value={value}
-        aria-label={['Fra kr', 'Til kr']}
-        aria-valuetext={[`${value[0]} kr`, `${value[1]} kr`]}
-      />
-      <div className="w-slider__input">
-        <TextField
-          label="Min"
-          value={value[0].toString()}
-          onChange={(e) => setValue([+e.target.value, value[1]])}
+      <div style={{ width: '320px', outline: '1px dashed red' }}>
+        <RangeSlider
+          onChange={setValue}
+          onInput={setOutput}
+          min={0}
+          max={100}
+          step={5}
+          value={value}
+          aria-label={['Fra kr', 'Til kr']}
+          aria-valuetext={[`${value[0]} kr`, `${value[1]} kr`]}
         />
-        <TextField
-          label="Max"
-          value={value[1].toString()}
-          onChange={(e) => setValue([value[0], +e.target.value])}
-        />
+        <div className="w-slider__input">
+          <TextField
+            label="Min"
+            value={value[0].toString()}
+            onChange={(e) => setValue([+e.target.value, value[1]])}
+          />
+          <TextField
+            label="Max"
+            value={value[1].toString()}
+            onChange={(e) => setValue([value[0], +e.target.value])}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -158,33 +169,35 @@ export const DisabledRanged = () => {
   ]);
   const [output, setOutput] = useState(value);
   return (
-    <div>
+    <>
       <output>
         {output[0]} - {output[1]} kr
       </output>
-      <RangeSlider
-        disabled
-        onChange={setValue}
-        onInput={setOutput}
-        min={1000000}
-        max={10000000}
-        step={50000}
-        value={value}
-      />
-      <div className="w-slider__input">
-        <TextField
-          label="Min"
+      <div style={{ width: '320px', outline: '1px dashed red' }}>
+        <RangeSlider
           disabled
-          value={value[0].toString()}
-          onChange={(e) => setValue([+e.target.value, value[1]])}
+          onChange={setValue}
+          onInput={setOutput}
+          min={1000000}
+          max={10000000}
+          step={50000}
+          value={value}
         />
-        <TextField
-          label="Max"
-          disabled
-          value={value[1].toString()}
-          onChange={(e) => setValue([value[0], +e.target.value])}
-        />
+        <div className="w-slider__input">
+          <TextField
+            label="Min"
+            disabled
+            value={value[0].toString()}
+            onChange={(e) => setValue([+e.target.value, value[1]])}
+          />
+          <TextField
+            label="Max"
+            disabled
+            value={value[1].toString()}
+            onChange={(e) => setValue([value[0], +e.target.value])}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };

af64c9f80c3b5acd41a4b7e87bd53bd693d9f331
adam-zawisza-jalaluddin-ahmed <adam.zawisza.jalaluddin.ahmed@schibsted.com>
2025-06-19 09:23:58 +0200

    Refactor

--- react/pageindicator/PageIndicator.tsx
+++ react/pageindicator/PageIndicator.tsx
@@ -10,12 +10,14 @@ export function PageIndicator(props: PageIndicatorProps) {
         {style}
       </style>
       <div className={'w-pageindicator'} style={customStyle}>
-        {[...Array(pageCount).keys()].map((i) => (
-          <div
-            className={`w-pageindicator--dot ${i === selectedPage - 1 ? 'w-pageindicator--selecteddot' : ''}`}
-            key={i}
-          />
-        ))}
+        <div className={'w-pageindicator--container'}>
+          {[...Array(pageCount).keys()].map((i) => (
+            <div
+              className={`w-pageindicator--dot ${i === selectedPage - 1 ? 'w-pageindicator--selecteddot' : ''}`}
+              key={i}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
